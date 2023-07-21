414986da1f3f587876faa1572110c1628a160fd2
EthanOk <sdzdj01@163.com>
2023-07-21 17:44:50 +0800

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   src/server.js

---
+++ src/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// 设置跨域访问
+app.use(cors());
+
+// 解析请求体
+app.use(bodyParser.json());
+
+// 创建MySQL数据库连接
+const db = mysql.createConnection({
+  host: "192.168.0.173",
+  user: "root",
+  password: "root",
+  port: "3306",
+  database: "aggregator_ethan",
+});
+
+// 连接到MySQL数据库
+db.connect((err) => {
+  if (err) {
+    console.error("MySQL连接错误:", err);
+  } else {
+    console.log("成功连接到MySQL数据库");
+  }
+});
+
+// 创建一个简单的API路由，用于从数据库中获取表数据
+app.get("/api/data", (req, res) => {
+  const sql = "SELECT * FROM system"; // 将"your_table_name"替换为你的表名
+
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.error("查询数据库错误：", err);
+      res.status(500).json({ error: "数据库查询错误" });
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+// 启动服务器，监听端口3001
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`服务器已启动，监听端口：${PORT}`);
+});
